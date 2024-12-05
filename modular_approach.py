b7cba58bd6dd8b2066e8e82252ec9ed493f31337
Will Thomas <30wthomas@gmail.com>
2024-12-05 13:31:16 -0600

    merge heurisitcs and modular approach

--- modular_approach.py
+++ modular_approach.py
@@ -171,10 +171,7 @@ def disp_image(image, title="Image"):
     cv2.destroyWindow(title)
 
 
-def remove_shadows(image_path):
-    # Load the image
-    img = cv2.imread(image_path)
-
+def remove_shadows(img: np.ndarray) -> np.ndarray:
     # Convert to the LAB color space to work on the lightness channel
     lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
     l, a, b = cv2.split(lab)
@@ -199,20 +196,83 @@ def remove_shadows(image_path):
     img1 = cv2.adaptiveThreshold(
         blur2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3
     )
-    disp_image(img1, "Adaptive Thresholding 1")
-
-    # # blur again
-    # blur3 = cv2.medianBlur(img1, 5)
-    # disp_image(blur3, "Median Blurred Image")
-
-    # # adaptive threshold 11, 3
-    # img2 = cv2.adaptiveThreshold(
-    #     blur3, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3
-    # )
-    # disp_image(img2, "Adaptive Thresholding 2")
 
     # Apply binary thresholding to segment shadow regions
     return img1
+
+
+def remove_center(orig_img: np.ndarray, shadowless_img: np.ndarray) -> None:
+    my_img = orig_img.copy()
+
+    my_img = cv2.cvtColor(my_img, cv2.COLOR_BGR2GRAY)
+    # apply threshold to mask IC housing
+    blur2 = cv2.bilateralFilter(my_img, 9, 75, 75)
+    _, th = cv2.threshold(blur2, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # blur = cv2.GaussianBlur(th, (51, 51), 0)
+    # _, th = cv2.threshold(blur, 110, 255, cv2.THRESH_BINARY)
+    # th = cv2.convertScaleAbs(th, alpha=1, beta=0).astype(np.int32)
+    contours, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    if len(contours) != 0:
+        # draw in blue the contours that were founded
+        print("Len Contours", len(contours))
+        sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        # c = max(contours, key=cv2.contourArea)
+        cv2.drawContours(shadowless_img, sorted_contours[:1], -1, (255, 255, 255), -1)
+        # x, y, w, h = cv2.boundingRect(c)
+
+
+def get_edges(shadowless_img: np.ndarray) -> np.ndarray:
+    edges = cv2.Canny(shadowless_img, 0, 257)
+    return edges
+
+
+def get_contours(edge_img: np.ndarray) -> list:
+    contours, hierarchy = cv2.findContours(
+        edge_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
+    print(f"Number of contours: {len(contours)}")
+
+    boxes = []  # each element is [[top-left], [bottom-right]];
+    hierarchy = hierarchy[0]
+    for currentContour, currentHierarchy in zip(contours, hierarchy):
+        x, y, w, h = cv2.boundingRect(currentContour)
+        if currentHierarchy[3] < 0:
+            # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            boxes.append([[x, y], [x + w, y + h]])
+
+    filtered = []
+    for box in boxes:
+        w = box[1][0] - box[0][0]
+        h = box[1][1] - box[0][1]
+        area = w * h
+        if 100 < area < 15000 and (8 < w) and (8 < h):
+            # if (5 < w < 200) and (5 < h < 200):
+            filtered.append(box)
+            # cv2.rectangle(img_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    boxes = sorted(
+        filtered,
+        key=(lambda x: (x[1][0] - x[0][0]) * (x[1][1] - x[0][1])),
+        reverse=True,
+    )
+    return boxes
+
+
+def draw_boxes(orig_img: np.ndarray, boxes: list) -> np.ndarray:
+    my_img = orig_img.copy()
+    for box in boxes:
+        cv2.rectangle(my_img, tup(box[0]), tup(box[1]), (0, 200, 0), 5)
+    return my_img
+
+
+def filter_boxes(boxes: list) -> list:
+    AREA_FILTER = 1000
+    filtered_boxes = []
+    for box in boxes:
+        [x1, y1], [x2, y2] = box
+        if (x2 - x1) * (y2 - y1) >= AREA_FILTER:
+            filtered_boxes.append(box)
+    return filtered_boxes
 
 
 # Example usage
@@ -223,88 +283,39 @@ image_corpus = [
     "./pins_images/C-T-28SOP-04F-SM.png",
 ]
 input_image = image_corpus[3]  # Path to the input image
-shadowless_img = remove_shadows(input_image)
+# NOTE: Throughout the functions that accept images will expect them to be in BGR form, and return in BGR form.
+# Everyone will responsible for their own conversions
+orig_img = cv2.imread(input_image)
 
-img = cv2.imread(input_image)
-img_rgb = cv2.imread(input_image)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+disp_image(orig_img)
 
-# apply threshold to mask IC housing
-blur2 = cv2.bilateralFilter(img, 9, 75, 75)
-_, th = cv2.threshold(blur2, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-# blur = cv2.GaussianBlur(th, (51, 51), 0)
-# _, th = cv2.threshold(blur, 110, 255, cv2.THRESH_BINARY)
-# th = cv2.convertScaleAbs(th, alpha=1, beta=0).astype(np.int32)
-contours, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+# Remove shadows from the image
+shadowless_img = remove_shadows(orig_img)
+disp_image(shadowless_img)
 
-if len(contours) != 0:
-    # draw in blue the contours that were founded
-    print("Len Contours", len(contours))
-    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    # c = max(contours, key=cv2.contourArea)
-    cv2.drawContours(shadowless_img, sorted_contours[:1], -1, (255, 255, 255), -1)
-    # x, y, w, h = cv2.boundingRect(c)
+# Remove the center from the image
+remove_center(orig_img, shadowless_img)
+disp_image(shadowless_img)
 
-    # # draw the biggest contour (c) in green
-    # cv2.rectangle(shadowless_img, (x, y), (x + w, y + h), (255, 255, 255), -1)
+# Detect edges
+edges = get_edges(shadowless_img)
+disp_image(edges)
 
-disp_image(shadowless_img, "Rect Image")
+# get initial boxes
+boxes = get_contours(edges)
 
-edges = cv2.Canny(shadowless_img, 0, 257)
-disp_image(edges, "Canny")
-
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-print(f"Number of contours: {len(contours)}")
-
-boxes = []  # each element is [[top-left], [bottom-right]];
-hierarchy = hierarchy[0]
-for currentContour, currentHierarchy in zip(contours, hierarchy):
-    x, y, w, h = cv2.boundingRect(currentContour)
-    if currentHierarchy[3] < 0:
-        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        boxes.append([[x, y], [x + w, y + h]])
-
-filtered = []
-for box in boxes:
-    w = box[1][0] - box[0][0]
-    h = box[1][1] - box[0][1]
-    area = w * h
-    if 100 < area < 15000 and (8 < w) and (8 < h):
-        # if (5 < w < 200) and (5 < h < 200):
-        filtered.append(box)
-        # cv2.rectangle(img_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
-boxes = sorted(
-    filtered, key=(lambda x: (x[1][0] - x[0][0]) * (x[1][1] - x[0][1])), reverse=True
-)
-
-print("# Boxes: ", len(boxes))
-# disp_image(img_rgb, "Bounding Boxes")
-
-# disp_image(cv2.merge([img, shadowless_img]), "Final Image")
-# disp_image(img, "Shadowless Image")
-
-img_initial_boxes = cv2.imread(input_image)
-img_filtered_boxes = cv2.imread(input_image)
-img_final = cv2.imread(input_image)
-
-# NOTE: We need to utilize a merging capability similar to what is shown in this SO post: https://stackoverflow.com/questions/66490374/how-to-merge-nearby-bounding-boxes-opencv
-# NOTE: second argument is merge_margin (affects allowed gaps)
+# Merge boxes
 boxes = merge(boxes, 1)
 
-for box in boxes:
-    cv2.rectangle(img_initial_boxes, tup(box[0]), tup(box[1]), (0, 200, 0), 5)
-disp_image(img_initial_boxes, "Bounding Boxes")
+# Draw on boxes
+boxed_img = draw_boxes(orig_img, boxes)
+disp_image(boxed_img)
 
-# Now we want to filter out any boxes that are too small
-AREA_FILTER = 1000
-filtered_boxes = []
-for box in boxes:
-    [x1, y1], [x2, y2] = box
-    if (x2 - x1) * (y2 - y1) >= AREA_FILTER:
-        filtered_boxes.append(box)
-for box in filtered_boxes:
-    cv2.rectangle(img_filtered_boxes, tup(box[0]), tup(box[1]), (0, 200, 0), 5)
-disp_image(img_filtered_boxes, "Bounding Boxes")
+# Filter boxes
+filt_boxes = filter_boxes(boxes)
 
+# Draw new boxes
+filt_boxed_img = draw_boxes(orig_img, filt_boxes)
+disp_image(filt_boxed_img)
 
 cv2.destroyAllWindows()
