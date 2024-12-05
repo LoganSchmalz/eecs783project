e5e864a7dca03cf90b31445d5c6565bb80a148fa
Logan Schmalz <loganschmalz@gmail.com>
2024-12-04 18:30:13 -0600

    add box merging

--- pins.py
+++ pins.py
@@ -8,8 +8,10 @@ def threshold(img, threshold_setting):
     titles = []
 
     # global thresholding
-    global_threshold_val = 127
-    ret1, th1 = cv2.threshold(img, global_threshold_val, 255, threshold_setting)
+    # blurglobal = cv2.GaussianBlur(img, (5, 5), 0)
+    blurglobal = cv2.bilateralFilter(img, 9, 75, 75)
+    global_threshold_val = 100
+    ret1, th1 = cv2.threshold(blurglobal, global_threshold_val, 255, threshold_setting)
     images.extend(
         [
             img,
@@ -43,7 +45,7 @@ def threshold(img, threshold_setting):
     )
 
     # Otsu's thresholding after Gaussian filtering
-    kernel_size = 3
+    kernel_size = 3  # 5
     blur = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
     ret3, th3 = cv2.threshold(blur, 0, 255, threshold_setting + cv2.THRESH_OTSU)
     images.extend(
@@ -62,23 +64,23 @@ def threshold(img, threshold_setting):
     )
 
     # Otsu's thresholding after bilateral filtering
-    sigma = 75  # 75
-    blur2 = cv2.bilateralFilter(img, 9, sigma, sigma)
-    ret4, th4 = cv2.threshold(blur2, 0, 255, threshold_setting + cv2.THRESH_OTSU)
-    images.extend(
-        [
-            blur2,
-            0,
-            th4,
-        ]
-    )
-    titles.extend(
-        [
-            "Bilateral filtered Image",
-            "Histogram",
-            "Otsu's Thresholding",
-        ]
-    )
+    # sigma = 75  # 75
+    # blur2 = cv2.bilateralFilter(img, 9, sigma, sigma)
+    # ret4, th4 = cv2.threshold(blur2, 0, 255, threshold_setting + cv2.THRESH_OTSU)
+    # images.extend(
+    #     [
+    #         blur2,
+    #         0,
+    #         th4,
+    #     ]
+    # )
+    # titles.extend(
+    #     [
+    #         "Bilateral filtered Image",
+    #         "Histogram",
+    #         "Otsu's Thresholding",
+    #     ]
+    # )
 
     # th5 = cv2.adaptiveThreshold(
     #     img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, threshold_setting, 11, 2
@@ -98,46 +100,44 @@ def threshold(img, threshold_setting):
     #     ]
     # )
 
-    th6 = cv2.adaptiveThreshold(
-        blur2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, threshold_setting, 11, 2
-    )
-    images.extend(
-        [
-            blur2,
-            0,
-            th6,
-        ]
-    )
-    titles.extend(
-        [
-            "Bilateral filtered Image",
-            "Histogram",
-            "Adaptive Thresholding",
-        ]
-    )
+    # th6 = cv2.adaptiveThreshold(
+    #     blur2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, threshold_setting, 11, 2
+    # )
+    # images.extend(
+    #     [
+    #         blur2,
+    #         0,
+    #         th6,
+    #     ]
+    # )
+    # titles.extend(
+    #     [
+    #         "Bilateral filtered Image",
+    #         "Histogram",
+    #         "Adaptive Thresholding",
+    #     ]
+    # )
 
     # plot all the images and their histograms
-    # i_max = int(len(images) / 3)
-    # for i in range(i_max):
-    #     plt.subplot(i_max, 3, i * 3 + 1), plt.imshow(images[i * 3], "gray")
-    #     plt.title(titles[i * 3]), plt.xticks([]), plt.yticks([])
-    #     plt.subplot(i_max, 3, i * 3 + 2), plt.hist(images[i * 3].ravel(), 256)
-    #     plt.title(titles[i * 3 + 1]), plt.xticks([]), plt.yticks([])
-    #     plt.subplot(i_max, 3, i * 3 + 3), plt.imshow(images[i * 3 + 2], "gray")
-    #     plt.title(titles[i * 3 + 2]), plt.xticks([]), plt.yticks([])
-    # plt.show()
-    # plt.cla()
+    i_max = int(len(images) / 3)
+    for i in range(i_max):
+        plt.subplot(i_max, 3, i * 3 + 1), plt.imshow(images[i * 3], "gray")
+        plt.title(titles[i * 3]), plt.xticks([]), plt.yticks([])
+        plt.subplot(i_max, 3, i * 3 + 2), plt.hist(images[i * 3].ravel(), 256)
+        plt.title(titles[i * 3 + 1]), plt.xticks([]), plt.yticks([])
+        plt.subplot(i_max, 3, i * 3 + 3), plt.imshow(images[i * 3 + 2], "gray")
+        plt.title(titles[i * 3 + 2]), plt.xticks([]), plt.yticks([])
+    plt.show()
+    plt.cla()
 
     return (ret4, th4)
 
 
 # img = cv2.imread("pins_images/A-D-64QFP-14B-SM.png")
-img = cv2.imread("pins_images/A-D-64QFP-15B-SM.png")
+# img = cv2.imread("pins_images/A-D-64QFP-15B-SM.png")
 # img = cv2.imread("pins_images/A-J-28SOP-01B-SM.png")
-# img = cv2.imread("pins_images/C-T-28SOP-04F-SM.png")
+img = cv2.imread("pins_images/C-T-28SOP-04F-SM.png")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-(h, w) = img.shape[:2]
-img = cv2.resize(img, (int(w / 4), int(h / 4)))
 
 _, th = threshold(img, cv2.THRESH_BINARY_INV)
 blur = cv2.GaussianBlur(th, (51, 51), 0)
@@ -159,23 +159,51 @@ if len(contours) != 0:
 
 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), -1)
 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 10)
-cv2.imshow("", img)
-cv2.waitKey(0)
+blur2 = cv2.bilateralFilter(img, 9, 75, 75)
+img = cv2.adaptiveThreshold(
+    blur2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+)
+
+
+(img_h, img_w) = img.shape[:2]
+# # Apply edge detection to highlight structures like metal pins
+for i in range(0, 255, 20):
+    for j in range(i, 255, 20):
+        edges = cv2.Canny(img, i, j)
+        # Find contours in the edge-detected image
+        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # Convert the original image to color to draw boxes
+        cv_image_color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        # Define a list to hold bounding boxes
+        bounding_boxes = []
+
+        # Loop through the contours and draw bounding boxes for detected objects
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            # Filter to ignore very small or very large boxes (noise or background)
+            if 10 < w < 100 and 10 < h < 100:  # Adjust size thresholds as needed
+                bounding_boxes.append((x, y, w, h))
+                cv2.rectangle(cv_image_color, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # cv2.imshow(f"{i}{j}", cv_image_color)
+        img = cv2.resize(img, (int(img_w / 2), int(img_h / 2)))
+        cv2.imshow(f"{i}{j}", img)
+        cv2.waitKey(0)
+        cv2.waitKey(0)
 
 # _, th = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
 # edges = cv2.Canny(img | th, 100, 200)
 # cv2.imshow("res2", edges)
 # cv2.waitKey(0)
 # _, th = cv2.threshold(img, 250, 0, cv2.THRESH_BINARY)
-cv2.imshow("", th)
-cv2.waitKey(0)
-contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-print(len(contours))
-print(contours)
-img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-cv2.drawContours(img, contours, -1, 255, 3)
-cv2.imshow("res2", img)
-cv2.waitKey(0)
+# cv2.imshow("", th)
+# cv2.waitKey(0)
+# contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+# print(len(contours))
+# print(contours)
+# img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+# cv2.drawContours(img, contours, -1, 255, 3)
+# cv2.imshow("res2", img)
+# cv2.waitKey(0)
 
 # cv2.imshow("res2", th)
 # cv2.waitKey(0)
--- shadow_killer.py
+++ shadow_killer.py
@@ -2,6 +2,112 @@ import cv2
 import numpy as np
 
 
+# merging boxes algorithm from https://stackoverflow.com/questions/66490374/
+# tuplify
+def tup(point):
+    return (point[0], point[1])
+
+
+# returns true if the two boxes overlap
+def overlap(source, target):
+    # unpack points
+    tl1, br1 = source
+    tl2, br2 = target
+    # checks
+    if tl1[0] >= br2[0] or tl2[0] >= br1[0]:
+        return False
+    if tl1[1] >= br2[1] or tl2[1] >= br1[1]:
+        return False
+    return True
+
+
+# returns all overlapping boxes
+def getAllOverlaps(boxes, bounds, index):
+    overlaps = []
+    for a in range(len(boxes)):
+        if a != index:
+            if overlap(bounds, boxes[a]):
+                overlaps.append(a)
+    return overlaps
+
+
+def merge(boxes, merge_margin, img=None):
+    # this is gonna take a long time
+    finished = False
+    highlight = [[0, 0], [1, 1]]
+    points = [[[0, 0]]]
+    while not finished:
+        # set end con
+        finished = True
+        # check progress
+        # print("Len Boxes: " + str(len(boxes)))
+        # draw boxes # comment this section out to run faster
+        if img is not None:
+            copy = np.copy(img)
+            for box in boxes:
+                cv2.rectangle(copy, tup(box[0]), tup(box[1]), (0, 200, 0), 1)
+            cv2.rectangle(copy, tup(highlight[0]), tup(highlight[1]), (0, 0, 255), 2)
+            for point in points:
+                point = point[0]
+                cv2.circle(copy, tup(point), 4, (255, 0, 0), -1)
+            cv2.imshow("Copy", copy)
+            key = cv2.waitKey(1)
+            if key == ord("q"):
+                break
+
+        # loop through boxes
+        index = len(boxes) - 1
+        while index >= 0:
+            # grab current box
+            curr = boxes[index]
+            # add margin
+            tl = curr[0][:]
+            br = curr[1][:]
+            tl[0] -= merge_margin
+            tl[1] -= merge_margin
+            br[0] += merge_margin
+            br[1] += merge_margin
+            # get matching boxes
+            overlaps = getAllOverlaps(boxes, [tl, br], index)
+            # check if empty
+            if len(overlaps) > 0:
+                # combine boxes
+                # convert to a contour
+                con = []
+                overlaps.append(index)
+                for ind in overlaps:
+                    tl, br = boxes[ind]
+                    con.append([tl])
+                    con.append([br])
+                con = np.array(con)
+                # get bounding rect
+                x, y, w, h = cv2.boundingRect(con)
+                # stop growing
+                w -= 1
+                h -= 1
+                merged = [[x, y], [x + w, y + h]]
+                # highlights
+                highlight = merged[:]
+                points = con
+                # remove boxes from list
+                overlaps.sort(reverse=True)
+                for ind in overlaps:
+                    del boxes[ind]
+                boxes.append(merged)
+                # set flag
+                finished = False
+                break
+
+            # increment
+            index -= 1
+    cv2.destroyAllWindows()
+
+    return boxes
+
+
+#####
+
+
 def disp_image(image, title="Image"):
     cv2.imshow(title, image)
     cv2.setWindowProperty(title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
@@ -53,134 +159,6 @@ def remove_shadows(image_path):
     return img1
 
 
-def threshold(img, threshold_setting):
-    images = []
-    titles = []
-
-    # global thresholding
-    global_threshold_val = 127
-    ret1, th1 = cv2.threshold(img, global_threshold_val, 255, threshold_setting)
-    images.extend(
-        [
-            img,
-            0,
-            th1,
-        ]
-    )
-    titles.extend(
-        [
-            "Original Noisy Image",
-            "Histogram",
-            f"Global Thresholding (v={global_threshold_val})",
-        ]
-    )
-
-    # Otsu's thresholding
-    ret2, th2 = cv2.threshold(img, 0, 255, threshold_setting + cv2.THRESH_OTSU)
-    images.extend(
-        [
-            img,
-            0,
-            th2,
-        ]
-    )
-    titles.extend(
-        [
-            "Original Noisy Image",
-            "Histogram",
-            "Otsu's Thresholding",
-        ]
-    )
-
-    # Otsu's thresholding after Gaussian filtering
-    kernel_size = 3
-    blur = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
-    ret3, th3 = cv2.threshold(blur, 0, 255, threshold_setting + cv2.THRESH_OTSU)
-    images.extend(
-        [
-            blur,
-            0,
-            th3,
-        ]
-    )
-    titles.extend(
-        [
-            "Gaussian filtered Image",
-            "Histogram",
-            "Otsu's Thresholding",
-        ]
-    )
-
-    # Otsu's thresholding after bilateral filtering
-    sigma = 75  # 75
-    blur2 = cv2.bilateralFilter(img, 9, sigma, sigma)
-    ret4, th4 = cv2.threshold(blur2, 0, 255, threshold_setting + cv2.THRESH_OTSU)
-    images.extend(
-        [
-            blur2,
-            0,
-            th4,
-        ]
-    )
-    titles.extend(
-        [
-            "Bilateral filtered Image",
-            "Histogram",
-            "Otsu's Thresholding",
-        ]
-    )
-
-    # th5 = cv2.adaptiveThreshold(
-    #     img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, threshold_setting, 11, 2
-    # )
-    # images.extend(
-    #     [
-    #         img,
-    #         0,
-    #         th5,
-    #     ]
-    # )
-    # titles.extend(
-    #     [
-    #         "Original Noisy Image",
-    #         "Histogram",
-    #         "Adaptive Thresholding",
-    #     ]
-    # )
-
-    th6 = cv2.adaptiveThreshold(
-        blur2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, threshold_setting, 11, 2
-    )
-    images.extend(
-        [
-            blur2,
-            0,
-            th6,
-        ]
-    )
-    titles.extend(
-        [
-            "Bilateral filtered Image",
-            "Histogram",
-            "Adaptive Thresholding",
-        ]
-    )
-
-    # plot all the images and their histograms
-    # i_max = int(len(images) / 3)
-    # for i in range(i_max):
-    #     plt.subplot(i_max, 3, i * 3 + 1), plt.imshow(images[i * 3], "gray")
-    #     plt.title(titles[i * 3]), plt.xticks([]), plt.yticks([])
-    #     plt.subplot(i_max, 3, i * 3 + 2), plt.hist(images[i * 3].ravel(), 256)
-    #     plt.title(titles[i * 3 + 1]), plt.xticks([]), plt.yticks([])
-    #     plt.subplot(i_max, 3, i * 3 + 3), plt.imshow(images[i * 3 + 2], "gray")
-    #     plt.title(titles[i * 3 + 2]), plt.xticks([]), plt.yticks([])
-    # plt.show()
-    # plt.cla()
-
-    return (ret4, th4)
-
-
 # Example usage
 image_corpus = [
     "./pins_images/A-J-28SOP-01B-SM.png",
@@ -188,14 +166,16 @@ image_corpus = [
     "./pins_images/A-D-64QFP-15B-SM.png",
     "./pins_images/C-T-28SOP-04F-SM.png",
 ]
-input_image = image_corpus[0]  # Path to the input image
+input_image = image_corpus[3]  # Path to the input image
 shadowless_img = remove_shadows(input_image)
 
 img = cv2.imread(input_image)
 img_rgb = cv2.imread(input_image)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-_, th = threshold(img, cv2.THRESH_BINARY_INV)
+# apply threshold to mask IC housing
+blur2 = cv2.bilateralFilter(img, 9, 75, 75)
+_, th = cv2.threshold(blur2, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 # blur = cv2.GaussianBlur(th, (51, 51), 0)
 # _, th = cv2.threshold(blur, 110, 255, cv2.THRESH_BINARY)
 # th = cv2.convertScaleAbs(th, alpha=1, beta=0).astype(np.int32)
@@ -220,22 +200,39 @@ disp_image(edges, "Canny")
 contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 print(f"Number of contours: {len(contours)}")
 
-bounding_boxes = []
-for contour in contours:
-    x, y, w, h = cv2.boundingRect(contour)
+boxes = []  # each element is [[top-left], [bottom-right]];
+hierarchy = hierarchy[0]
+for currentContour, currentHierarchy in zip(contours, hierarchy):
+    # currentContour = component[0]
+    # currentHierarchy = component[1]
+    x, y, w, h = cv2.boundingRect(currentContour)
+    if currentHierarchy[3] < 0:
+        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        boxes.append([[x, y], [x + w, y + h]])
+
+filtered = []
+for box in boxes:
+    w = box[1][0] - box[0][0]
+    h = box[1][1] - box[0][1]
     area = w * h
     if 100 < area < 15000 and (7 < w) and (7 < h):
         # if (5 < w < 200) and (5 < h < 200):
-        bounding_boxes.append((x, y, w, h))
-        cv2.rectangle(img_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        filtered.append(box)
+        # cv2.rectangle(img_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
+boxes = filtered
 
-print(len(bounding_boxes))
+print(len(boxes))
 disp_image(img_rgb, "Bounding Boxes")
 
 # disp_image(cv2.merge([img, shadowless_img]), "Final Image")
 # disp_image(img, "Shadowless Image")
 
 # NOTE: We need to utilize a merging capability similar to what is shown in this SO post: https://stackoverflow.com/questions/66490374/how-to-merge-nearby-bounding-boxes-opencv
-
-
+# NOTE: second argument is merge_margin (affects allowed gaps)
+boxes = merge(boxes, 0)
+for box in boxes:
+    cv2.rectangle(img_rgb, tup(box[0]), tup(box[1]), (0, 200, 0), 5)
+(img_rgb_h, img_rgb_w) = img_rgb.shape[:2]
+img_rgb = cv2.resize(img_rgb, (int(img_rgb_w / 3), int(img_rgb_h / 3)))
+disp_image(img_rgb, "Bounding Boxes")
 cv2.destroyAllWindows()
