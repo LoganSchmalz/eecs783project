9fbbf987a3aff2c09cebe8650e160462dd928026
Logan Schmalz <loganschmalz@gmail.com>
2024-12-07 18:57:50 -0600

    mess around with some preprocessing for marking

--- marking.py
+++ marking.py
@@ -6,6 +6,15 @@ import os
 import glob
 
 
+def disp_image(image, title="Image", save_image=False):
+    (h, w) = image.shape[:2]
+    img = cv2.resize(image, (int(w / 4), int(h / 4)))
+    cv2.imshow(title, img)
+    cv2.setWindowProperty(title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.waitKey(0)
+    cv2.destroyWindow(title)
+
+
 def threshold(img, threshold_setting):
     images = []
     titles = []
@@ -133,17 +142,24 @@ def threshold(img, threshold_setting):
 
     return (ret4, th4)
 
+
 # contrast function from @/pietz on StackOverflow:
-def adjust_contrast_brightness(img, contrast:float=1.0, brightness:int=0):
+def adjust_contrast_brightness(img, contrast: float = 1.0, brightness: int = 0):
     """
     Adjusts contrast and brightness of an uint8 image.
     contrast:   (0.0,  inf) with 1.0 leaving the contrast as is
     brightness: [-255, 255] with 0 leaving the brightness as is
     """
-    brightness += int(round(255*(1-contrast)/2))
+    brightness += int(round(255 * (1 - contrast) / 2))
     return cv2.addWeighted(img, contrast, img, 0, brightness)
 
-img_list = ["marking_images/A-J-28SOP-03F-SM.png", "marking_images/C-T-08DIP-11F-SM.png", "marking_images/C-T-48QFP-19F-SM.png", "marking_images/C-T-48QFP-20F-SM.png"]
+
+img_list = [
+    "marking_images/A-J-28SOP-03F-SM.png",
+    "marking_images/C-T-08DIP-11F-SM.png",
+    "marking_images/C-T-48QFP-19F-SM.png",
+    "marking_images/C-T-48QFP-20F-SM.png",
+]
 
 # img = cv2.imread("marking_images/A-J-28SOP-03F-SM.png")
 # img = cv2.imread("marking_images/C-T-08DIP-11F-SM.png")
@@ -151,39 +167,51 @@ img_list = ["marking_images/A-J-28SOP-03F-SM.png", "marking_images/C-T-08DIP-11F
 # img = cv2.imread("marking_images/C-T-48QFP-20F-SM.png")
 
 temp = 1
-for i in img_list: 
+for i in img_list:
     img = cv2.imread(i)
+    # Logan messing with preprocessing starts here
+    disp_image(img, "")
+    blur2 = cv2.bilateralFilter(img, 9, 150, 150)
+    disp_image(blur2, "")
+    blur2 = cv2.cvtColor(blur2, cv2.COLOR_BGR2GRAY)
+    disp_image(blur2, "")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    (h, w) = img.shape[:2]
-    img = cv2.resize(img, (int(w / 4), int(h / 4)))
+    img = blur2
+    # messing with preprocessing ends here
     _, th = threshold(img, cv2.THRESH_BINARY_INV)
     blur = cv2.GaussianBlur(th, (51, 51), 0)
     _, th = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)
     img = img & th
 
     # -------- comment out this section to get rid of contrast
-    cv2.imshow("", img) #shows image after threshholding and before adjusting brightness/contrast
+    disp_image(
+        img, ""
+    )  # shows image after threshholding and before adjusting brightness/contrast
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    img = adjust_contrast_brightness(img, 2, 25) # I arbitrarily chose numbers and adjusted from there, feel free to edit
+    img = adjust_contrast_brightness(
+        img, 2, 25
+    )  # I arbitrarily chose numbers and adjusted from there, feel free to edit
     # ---------- ^^
 
-    os.chdir("th_imgs") # change directory to th_imgs folder
-    cv2.imwrite("thImg_{}.png".format(temp), img) # save the updated thresholded image
-    cv2.imshow("", img) #shows image after adjusting brightness/contrast
+    os.chdir("th_imgs")  # change directory to th_imgs folder
+    cv2.imwrite("thImg_{}.png".format(temp), img)  # save the updated thresholded image
+    disp_image(img, "")  # shows image after adjusting brightness/contrast
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    temp = temp+1
-    os.chdir("..") 
+    temp = temp + 1
+    os.chdir("..")
 
 
-th_img_list = glob.glob("th_imgs/*.png") # create a list of all images with .png filetype in the th_imgs folder
+th_img_list = glob.glob(
+    "th_imgs/*.png"
+)  # create a list of all images with .png filetype in the th_imgs folder
 # print(th_img_list)
 
-reader = easyocr.Reader(['en']) # OCR reader object
+reader = easyocr.Reader(["en"])  # OCR reader object
 
 for img in th_img_list:
-    result = reader.readtext(img) 
-    print("\nMarkings on "+img, ": \n")
+    result = reader.readtext(img)
+    print("\nMarkings on " + img, ": \n")
     for detection in result:
-        print(detection[1]) # for all imgs in list, read text with OCR reader and print
+        print(detection[1])  # for all imgs in list, read text with OCR reader and print
